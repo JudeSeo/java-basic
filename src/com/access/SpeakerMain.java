8f2260e5352a41998031e43577d80a802feb44ba
서혜주 <hjseo@entropykorea.com>
2025-04-14 14:50:03 +0900

    접근 제어자 이해1

---
+++ src/com/access/SpeakerMain.java
@@ -0,0 +1,18 @@
+package com.access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolumn();
+
+        speaker.volumnUp();
+        speaker.showVolumn();
+
+        speaker.volumnUp();
+        speaker.showVolumn();
+
+        System.out.println("volumn 필드 직접 접근 수정");
+        speaker.volumn = 200;
+        speaker.showVolumn();
+    }
+}
